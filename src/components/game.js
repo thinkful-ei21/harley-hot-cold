f8eeb65ac2561775e3e882c6f71171c964be6157
harleyjj <harleyyesm@gmail.com>
2018-06-24 00:32:15 -0700

    finished implementing functionality

--- src/components/game.js
+++ src/components/game.js
@@ -10,8 +10,25 @@ export default class Game extends React.Component {
         super(props);
 
         this.state = {
-            clicked: ''
+            clicked: '',
+            feedback: 'Make your guess!',
+            count: 0,
+            guessList: [],
+            answer: this.getRandomIntInclusive(0,100)
         }
+    }
+
+    getRandomIntInclusive(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+      }
+
+    generateAnswer(){
+        const answer = this.getRandomIntInclusive(1,100);
+        this.setState({
+            answer
+        });
     }
 
     setClicked(clicked){
@@ -24,17 +41,51 @@ export default class Game extends React.Component {
     }
 
     resetState() {
-        console.log('state will be reset');
+        this.setState({
+            clicked: '',
+            feedback: 'Make your guess!',
+            count: 0,
+            guessList: [],
+            answer: this.getRandomIntInclusive(0,100)
+        });
+    }
+
+    updateState(guess) {
+        let feedback;
+        const distance = Math.abs(guess - this.state.answer);
+        if (distance === 0) {
+            feedback = 'You win!'
+        } else if (distance < 25) {
+            feedback = 'hot';
+        } else {
+            feedback = 'cold';
+        }
+        this.setState({
+            ...this.state, 
+            guessList:[...this.state.guessList, guess],
+            feedback,
+            count:this.state.count + 1
+        })
+    }
+
+    addGuess(guess){
+        const numGuess = parseInt(guess);
+        console.log(numGuess);
+        if (numGuess >= 1 && numGuess <= 100) {
+            this.updateState(numGuess);
+        }
     }
 
     render() {
+
+        console.log(this.state);
         
         return (
             <div>
                 <Header clicked={value => this.setClicked(value)} show={this.state.clicked === 'what' ? true : false}/>
-                <GuessSection feedback="Make your guess!" />
-                <GuessCount count={3} />
-                <GuessList guesses={[10, 15, 25]} />
+                <GuessSection feedback={this.state.feedback} getGuess={value=>this.addGuess(value)}/>
+                <GuessCount count={this.state.count} />
+                <GuessList guesses={this.state.guessList} />
             </div>
         );
     }
--- src/components/guess-form.js
+++ src/components/guess-form.js
@@ -2,14 +2,30 @@ import React from 'react';
 
 import './guess-form.css';
 
-export default function GuessForm(props) {
-    return (
-        <form>
-            <input type="text" name="userGuess" id="userGuess"
-                className="text" maxLength="3" autoComplete="off"
-                placeholder="Enter your Guess" required />
-            <input type="submit" id="guessButton" className="button" name="submit" value="Guess"/>
-        </form>
-    );
+export default class GuessForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    onSubmit(event) {
+        event.preventDefault();
+        const text = this.textInput.value.trim();
+        this.props.getGuess(text);
+        this.textInput.value='';
+    }
+
+    render() {
+        return (
+            <form onSubmit={e=>this.onSubmit(e)}>
+                <input type="text" name="userGuess" id="userGuess"
+                    className="text" maxLength="3" autoComplete="off"
+                    placeholder="Enter your Guess" required 
+                    ref={input=> this.textInput = input}/>
+                <input type="submit" id="guessButton" className="button" name="submit" value="Guess"/>
+            </form>
+        );
+    }
+
 };
 
